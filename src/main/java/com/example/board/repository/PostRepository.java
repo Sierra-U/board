b6076f6acbbf39398e1fb6c2f474d94fd2e51639
atsushi <chiroru.mei@gmail.com>
2022-05-26 17:53:59 +0900

    機能の追加

--- src/main/java/com/example/board/repository/PostRepository.java
+++ src/main/java/com/example/board/repository/PostRepository.java
@@ -1,11 +1,16 @@
 package com.example.board.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository;
 
 /*
  * 投稿のリポジトリー
  */
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+	public Optional<Post> findById(String id);
+	//public List<Post> findAll();
 }
